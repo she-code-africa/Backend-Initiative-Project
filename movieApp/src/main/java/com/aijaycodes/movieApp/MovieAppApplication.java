a47a71fb009a0189e98d7636208bc8cfdb347fdb
Linda <linda@users.noreply.gitbash.com>
2020-12-15 14:13:08 +0100

    Task First Commit.

---
+++ movieApp/src/main/java/com/aijaycodes/movieApp/MovieAppApplication.java
@@ -0,0 +1,13 @@
+package com.aijaycodes.movieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppApplication.class, args);
+	}
+
+}
---
+++ movieApp/src/test/java/com/aijaycodes/movieApp/MovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aijaycodes.movieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
