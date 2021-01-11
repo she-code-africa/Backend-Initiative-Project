290aaa410f9400d9876fca2792a565511029c326
ebosetalee <princess01ella.miley@gmail.com>
2021-01-11 12:30:29 +0100

    Errors fixed

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def home():
 @app.route("/login", methods=["POST"])
 def login():
     session.permanent = True
-    user = request.form["Full Name"]
+    user = request.form["fullname"]
     for find_user in users:
         if find_user == user:
             session["user"] = find_user
@@ -34,10 +34,11 @@ def user():
     if "user" in session:
         user = session["user"]
         if request.method == "PUT":
-            update_user = request.form["Update User"]
+            update_user = request.form["updateuser"]
             for index, item in enumerate(users):
                 if item == user:
                     users[index] = update_user
+                    session["user"] = update_user
                     return jsonify({"status": "Success", "user": "Updated {}".format(users[index])})
             return jsonify({"status": "Failure", "ERROR": "User not found"})
         elif request.method == "DELETE":
@@ -45,7 +46,7 @@ def user():
                 users.remove(user)
                 return jsonify({"status": "Success"})
             except:
-                return jsonify({"status": "Failure", "ERROR": "User not found"})
+                return jsonify({"status": "Failure", "ERROR": "User not found"}), 400
         return jsonify({"status": "Success", "user": "Welcome Back {}".format(user)})
     else:
         return jsonify({"status": "Failure", "ERROR": "User not found"})
@@ -55,7 +56,7 @@ def user():
 def movie():
     if request.method == "POST":
         session.permanent = True
-        movie = request.form["Movie"]
+        movie = request.form["movie"]
         for find_movie in movies:
             if find_movie == movie:
                 session["movie"] = find_movie
@@ -69,10 +70,11 @@ def movie():
             if request.method == "GET":
                 return jsonify({"status": "Success", "movie": movie})
             elif request.method == "PUT":
-                update_movie = request.form["Update Movie"]
+                update_movie = request.form["updatemovie"]
                 for index, movie_name in enumerate(movies):
                     if movie_name == movie:
                         movies[index] = update_movie
+                        session["movie"] = update_movie
                         return jsonify({"status": "Success", "movie Updated": movies[index]})
                 return jsonify({"status": "Failure", "ERROR": "Movie is Unavailable"}), 400
             elif request.method == "DELETE":
@@ -86,9 +88,9 @@ def movie():
 def rental():
     if request.method == "POST":
         session.permanent = True
-        rental = request.form["Rental"]
-        for find_rental in movies:
-            if find_rental == movie:
+        rental = request.form["rental"]
+        for find_rental in rentals:
+            if find_rental == rental:
                 session["rental"] = find_rental
                 return redirect(url_for("rental", rent=rental)), 200
         rentals.append(rental)
@@ -100,17 +102,18 @@ def rental():
             if request.method == "GET":
                 return jsonify({"status": "Success", "Rental Service": rental})
             elif request.method == "PUT":
-                update_rental = request.form["Update Rental"]
+                update_rental = request.form["updaterental"]
                 for index, rental_name in enumerate(rentals):
                     if rental_name == rental:
                         rentals[index] = update_rental
+                        session["rental"] = update_rental
                         return jsonify({"status": "Success", "movie Updated": rentals[index]})
                 return jsonify({"status": "Failure", "ERROR": "Rental Service is Unavailable"}), 400
             elif request.method == "DELETE":
                 rentals.remove(rental)
                 return jsonify({"status": "Success", "data": rentals})
         else:
-            return jsonify({"status": "Failure", "ERROR": "Movie not found", "Movies available": rentals}), 200
+            return jsonify({"status": "Welcome", "Rentals available": rentals}), 200
 
 
 if __name__ == "__main__":
