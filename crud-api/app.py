edbc1f965ff3d2d6c64a7b138146d9caa34446c0
thayeylolu <hamdallahhassanah@gmail.com>
2021-01-25 20:36:16 +0100

    made the finally correct crud

--- crud-api/app.py
+++ crud-api/app.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 import json
-from fastapi.encoders import jsonable_encoder
 import uvicorn
 from models.model import Rentals, Users, Movies
+
 # Init
 app = FastAPI()
-
 
 # read the files:
 with open("data/movies.json") as movies_content:
@@ -17,21 +16,21 @@ with open("data/users.json") as users_content:
 with open("data/rentals.json") as rentals_content:
     rentalsContent = json.load(rentals_content)
 
-
 # 1. GET ALL - READ
-@app.get('/movies')
+@app.get("/movies")
 async def allMovies():
     return [movie for movie in moviesContent]
 
 
-@app.get('/users')
+@app.get("/users")
 async def allUsers():
     return [user for user in usersContent]
 
 
-@app.get('/rentals')
+@app.get("/rentals")
 async def allRentals():
     return [rental for rental in rentalsContent]
+
 
 # 2  CREATE
 
@@ -43,75 +42,115 @@ def write_json(filename, value, req_body):
         # appending data to emp_details
         temp.append(req_body.dict())
 
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         json.dump(data, f, indent=4)
 
 
-@app.post('/addRentals')
+@app.post("/addRentals")
 async def create_rental(rental: Rentals):
-    write_json('data/rentals.json', 'rentals', rental)
+    write_json("data/rentals.json", "rentals", rental)
     return rental.dict()
 
 
-@app.post('/addMovies')
+@app.post("/addMovies")
 async def create_movies(movie: Movies):
-    write_json('data/movies.json', 'movies', movie)
+    write_json("data/movies.json", "movies", movie)
     return movie.dict()
 
 
-@app.post('/addUsers')
+@app.post("/addUsers")
 async def create_users(user: Users):
-    write_json('data/users.json', 'users', user)
+    write_json("data/users.json", "users", user)
     return user.dict()
 
-# 3. PUT
 
-
-def update_json1(filename, value, req_body, check_id, json_id):
-    with open(filename) as json_file:
-        data = json.load(json_file)
-        temp = data[str(value)]
-        for item in temp:
-            if item[json_id] == check_id:
-                item = req_body.dict()
-                return(item)
-        # appending data to emp_detail
-        temp.append(item)
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
-
-def update (item, data ):
-    pass
-    
-
-
-# 5 . GET A USER
-def get_json(filename, value, check_id,json_id):
+# 3 . GET A USER
+def get_json(filename, value, check_id, json_id):
     with open(filename) as json_file:
         data = json.load(json_file)
         temp = data[str(value)]
         for item in temp:
             if item[json_id] == check_id:
                 return item
+            else:
+                return "{}  does not exist in {} ".format(json_id, value)
 
 
-@app.get('/getUser/{userid}')
+@app.get("/getUser/{userid}")
 async def get_user(userid: int):
-    return get_json('data/users.json', 'users', userid, 'userId')
+    return get_json("data/users.json", "users", userid, "userId")
 
-@app.get('/getMovie/{movieid}')
+
+@app.get("/getMovie/{movieid}")
 async def get_movie(movieid: int):
-    return get_json('data/movies.json', 'movies', movieid, 'movieId')
+    return get_json("data/movies.json", "movies", movieid, "movieId")
 
-@app.get('/getRental/{rentalid}')
+
+@app.get("/getRental/{rentalid}")
 async def get_rental(rentalid: int):
-    return get_json('data/rentals.json', 'rentals', rentalid, 'rentalId')
+    return get_json("data/rentals.json", "rentals", rentalid, "rentalId")
 
 
-@app.put('/editUsers/{userid}' , response_model = Users)
+# 4. PUT
+
+
+def update_json(filename, value, json_id, value_id, updates):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        temp = data[str(value)]
+        for item in temp:
+            if item[str(json_id)] == value_id:
+                temp[temp.index(item)] = updates
+            else:
+                return "{}  does not exist in {} ".format(value_id, value)
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+    return updates
+
+
+@app.put("/editMovies/{movieid}", response_model=Movies)
+async def update_movie(movieid: int, movie: Movies):
+    return update_json("data/movies.json", "movies", "movieId", movieid, movie.dict())
+
+
+@app.put("/editUsers/{userid}", response_model=Users)
 async def update_user(userid: int, user: Users):
-    item = get_json('data/users.json', 'users', userid, 'userId')
-    user =  jsonable_encoder(item)
-    
-    return user
-    
+    return update_json("data/users.json", "users", "userId", userid, user.dict())
+
+
+@app.put("/editRentals/{rentalid}", response_model=Rentals)
+async def update_rental(rentalid: int, rental: Rentals):
+    return update_json(
+        "data/rentals.json", "rentals", "rentalId", rentalid, rental.dict()
+    )
+
+
+# 5. DELETE USER
+
+def delete_json(filename, value, json_id, value_id):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        temp = data[str(value)]
+        for item in temp:
+            if item[str(json_id)] == value_id:
+                del temp[temp.index(item)]
+            else:
+                return "{}  does not exist in {} ".format(value_id, value)
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+    return "deleted"
+
+
+@app.delete("/delMovies/{movieid}")
+async def delete_movie(movieid: int):
+    return delete_json("data/movies.json", "movies", "movieId", movieid)
+
+
+@app.delete("/delUsers/{userid}")
+async def delete_user(userid: int):
+    return delete_json("d ata/users.json", "users", "userId", userid)
+
+
+@app.delete("/delRentals/{rentalid}")
+async def delete_rental(rentalid: int):
+    return delete_json("data/rentals.json", "rentals", "rentalId", rentalid)
--- crud-api/data/d.py
+++
@@ -1,7 +0,0 @@
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
-
-print (items["bar"]) 
